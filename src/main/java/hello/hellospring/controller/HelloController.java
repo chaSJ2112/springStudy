bf79ed29fa4b8535c01f22ef6b6ec6ca229c1437
차수진 <tnwls4120@naver.com>
2021-09-20 13:54:41 +0900

    초기 설정 완료

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,54 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+	// /hello 라고 주소가 들어오면 다음이 실행.
+	@GetMapping("hello")
+	public String hello(Model model) {
+		model.addAttribute("data", "hello!");
+
+		return "hello";
+	}
+
+	@GetMapping("hello-mvc")
+	public String helloMvc(@RequestParam("name") String name, Model model) {
+		model.addAttribute("name", name);
+
+		return "hello-template";
+	}
+
+	@GetMapping("hello-spring")
+	@ResponseBody
+	public String helloSpring(@RequestParam("name") String name, Model model) {
+		return "hello" + name;
+	}
+
+	@GetMapping("hello-api")
+	@ResponseBody
+	public Hello helloApi(@RequestParam("name") String name) {
+		Hello hello = new Hello();
+		hello.setName(name);
+
+		return hello;
+	}
+
+	static class Hello {
+		private String name;
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+	}
+}
