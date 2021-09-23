ac2b2d90c95fc3d14e8fa5829949db6a830a3b41
차수진 <tnwls4120@naver.com>
2021-09-23 11:00:17 +0900

    feat : MemberService 클래스 생성

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -1,20 +1,53 @@
 package hello.hellospring.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 
 public class MemberService {
-	private final MemberRepository memberRepository = new MemoryMemberRepository();
-	
+	private final MemberRepository memberRepository;
+
+	public MemberService(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository; 
+	}
+
 	/**
-	 * 회원 가입
-	 * @param member
-	 * @return
+	 * 회원 가입.
 	 */
-	/*
-	 * public Long join(Member member) { memberRepository.save(member);
-	 * 
-	 * return }
+	public Long join(Member member) {
+		// 같은 이름은 가입할 수 없도록 한다.
+		/*
+		Optional<Member> result = memberRepository.findByName(member.getName());
+
+		result.ifPresent(m -> {
+			throw new IllegalStateException("이미 존재하는 회원입니다.");
+		});
+		*/
+		// 이것은 method로 뽑아주는게 좋다.
+		vaildateDuplicateMember(member);
+		
+		memberRepository.save(member);
+		return member.getId();
+	}
+
+	private void vaildateDuplicateMember(Member member) {
+		memberRepository.findByName(member.getName())
+		.ifPresent(m -> {
+			throw new IllegalStateException("이미 존재하는 회원입니다.");
+		});
+	}
+
+	/**
+	 * 전체 회원 조회
 	 */
+	public List<Member> findMembers() {
+		return memberRepository.findAll();
+	}
+
+	public Optional<Member> findOne(Long memberId) {
+		return memberRepository.findById(memberId);
+	}
 }
